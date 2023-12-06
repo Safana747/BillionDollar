b8840147422613dee2d3f217c0daf28744a29328
muhammedanshifpoothanari <121426946+muhammedanshifpoothanari@users.noreply.github.com>
2023-12-06 11:21:08 +0530

    Update App.jsx
    added a new component in app.jsx by muhammedanshifp

--- pixel_images_client/src/App.jsx
+++ pixel_images_client/src/App.jsx
@@ -1,7 +1,9 @@
 import "./App.css";
 import * as React from "react";
 import { useAreaSelection, useSelected } from "./area-selection";
+import ImageMixerDuringPayment from "./ImageMixerDuringPayment";
 import ImageMixer from "./ImageMixer";
+
 import Grid from "./Grid";
 import { useSelector } from "react-redux";
 import { Route, Routes } from "react-router";
@@ -17,6 +19,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Image />} />
         <Route path="/upload" element={<Grid />} />
+        <Route path="/payment_gateway" element={<ImageMixerDuringPayment />} />
         <Route path="/image" element={<ImageMixer />} />
       </Routes>
     </div>
