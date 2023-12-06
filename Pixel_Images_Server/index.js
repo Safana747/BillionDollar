45195b6f2c3f77bf14b1b9d38f3e60bc47d86bab
muhammedanshifpoothanari <121426946+muhammedanshifpoothanari@users.noreply.github.com>
2023-12-06 11:33:13 +0530

    Update index.js
    added cors to index.js by muhammedanshifp

--- Pixel_Images_Server/index.js
+++ Pixel_Images_Server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const dotenv = require("dotenv").config();
+
 const router = require("./routes");
 app.use(express.json());
 app.use(cors());
