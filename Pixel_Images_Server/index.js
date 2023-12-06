ed1dab24c6659ac602ff68e7d920fc1390911525
muhammedanshifpoothanari <121426946+muhammedanshifpoothanari@users.noreply.github.com>
2023-12-06 11:09:41 +0530

    Update index.js

--- Pixel_Images_Server/index.js
+++ Pixel_Images_Server/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const dotenv = require("dotenv").config();
-
 const router = require("./routes");
 app.use(express.json());
 app.use(cors());
